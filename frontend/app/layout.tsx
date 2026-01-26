4edc7d1f5ba376b2e2130252f2beed03dee574fb
johndoe6345789 <john.doe6345789@gmail.com>
2026-01-26 04:27:20 +0000

    Add script tag for env.js in layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <script src="/env.js" />
       </head>
       <body>
         <ThemeProvider>
